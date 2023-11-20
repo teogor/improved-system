dc451308563bcdb2f3fd88db6cee1db24424d7f7
Teodor Grigor <open-source@teogor.dev>
2023-11-20 21:24:10 +0200

    Update build.gradle.kts

--- examples/demo/build.gradle.kts
+++ examples/demo/build.gradle.kts
@@ -1,7 +1,3 @@
-import dev.teogor.xenoglot.Country
-import dev.teogor.xenoglot.Language
-import dev.teogor.xenoglot.territorialize
-
 plugins {
   id("kotlin")
   id("application")
@@ -10,21 +6,6 @@ plugins {
 }
 
 querent {
-  languagesSchemaOptions {
-    unqualifiedResLocale = Language.English territorialize Country.UnitedStates
-    addSupportedLanguages {
-      +(Language.Romanian territorialize Country.Romania)
-      +(Language.English territorialize Country.UnitedKingdom)
-      +(Language.Korean territorialize Country.SouthKorea)
-      +(Language.Dutch territorialize Country.Netherlands)
-      +(Language.German territorialize Country.Germany)
-      +(Language.Chinese territorialize Country.China)
-      +Language.Japanese
-      +Language.Spanish
-      +Language.Hindi
-      +Language.Arabic
-    }
-  }
 }
 
 application {
