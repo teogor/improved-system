f29833a7b907682dcc4253d9cf4aab9ef7d75a08
Teodor Grigor <open-source@teogor.dev>
2023-11-21 00:50:21 +0200

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import com.diffplug.spotless.LineEnding
 import com.vanniktech.maven.publish.SonatypeHost
 import dev.teogor.winds.api.MavenPublish
 import dev.teogor.winds.api.getValue
@@ -169,18 +170,21 @@ subprojects {
         licenseHeaderFile(rootProject.file("spotless/copyright.kt"))
         trimTrailingWhitespace()
         endWithNewline()
+        lineEndings = LineEnding.WINDOWS
       }
       format("kts") {
         target("**/*.kts")
         targetExclude("**/build/**/*.kts")
         // Look for the first line that doesn't have a block comment (assumed to be the license)
         licenseHeaderFile(rootProject.file("spotless/copyright.kts"), "(^(?![\\/ ]\\*).*$)")
+        lineEndings = LineEnding.WINDOWS
       }
       format("xml") {
         target("**/*.xml")
         targetExclude("**/build/**/*.xml")
         // Look for the first XML tag that isn't a comment (<!--) or the xml declaration (<?xml)
         licenseHeaderFile(rootProject.file("spotless/copyright.xml"), "(<[^!?])")
+        lineEndings = LineEnding.WINDOWS
       }
     }
   }
