8dfc6a8ef68f27fd0f7896e5264f7eac7a5449aa
Teodor Grigor <open-source@teogor.dev>
2023-11-20 21:36:37 +0200

    rev

--- querent/settings.gradle.kts
+++ querent/settings.gradle.kts
@@ -22,6 +22,8 @@ pluginManagement {
 }
 
 val projectDir: File = File(rootProject.projectDir.parent)
+
+@Suppress("UnstableApiUsage")
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
   repositories {
