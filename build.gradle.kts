d727398a6f7ef8b64a6962d3b218e4d9c9d1034c
Teodor Grigor <open-source@teogor.dev>
2023-11-20 20:10:49 +0200

    ewv

---
+++ build.gradle.kts
@@ -0,0 +1,25 @@
+buildscript {
+  repositories {
+    google()
+    mavenCentral()
+  }
+}
+
+// Lists all plugins used throughout the project without applying them.
+plugins {
+  alias(libs.plugins.android.application) apply false
+  alias(libs.plugins.kotlin.jvm) apply false
+  alias(libs.plugins.kotlin.serialization) apply false
+  alias(libs.plugins.firebase.crashlytics) apply false
+  alias(libs.plugins.firebase.perf) apply false
+  alias(libs.plugins.gms) apply false
+  alias(libs.plugins.hilt) apply false
+  alias(libs.plugins.ksp) apply false
+
+  alias(libs.plugins.vanniktech.maven)
+  alias(libs.plugins.winds)
+}
+
+winds {
+
+}
