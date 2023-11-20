a7a6ac3527fd1d93d6d83e5fb8a4e1b647aead9e
Teodor Grigor <open-source@teogor.dev>
2023-11-20 22:15:48 +0200

    rev

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,14 +18,8 @@ buildscript {
 
 // Lists all plugins used throughout the project without applying them.
 plugins {
-  alias(libs.plugins.android.application) apply false
   alias(libs.plugins.kotlin.jvm) apply false
   alias(libs.plugins.kotlin.serialization) apply false
-  alias(libs.plugins.firebase.crashlytics) apply false
-  alias(libs.plugins.firebase.perf) apply false
-  alias(libs.plugins.gms) apply false
-  alias(libs.plugins.hilt) apply false
-  alias(libs.plugins.ksp) apply false
 
   alias(libs.plugins.winds) apply true
   alias(libs.plugins.vanniktech.maven) apply true
--- examples/build.gradle.kts
+++ examples/build.gradle.kts
@@ -9,12 +9,7 @@ buildscript {
 plugins {
   // Kotlin Suite
   alias(libs.plugins.kotlin.jvm) apply false
-  alias(libs.plugins.kotlin.android) apply false
   alias(libs.plugins.kotlin.serialization) apply false
-
-  // Android Plugins
-  alias(libs.plugins.android.application) apply false
-  alias(libs.plugins.android.library) apply false
 
   alias(libs.plugins.vanniktech.maven) apply false
 }
